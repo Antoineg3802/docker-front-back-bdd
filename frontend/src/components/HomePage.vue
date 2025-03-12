1bbe704f08eb89747bbe2e72436f51ef15f3dd8c
Antoineg3802 <antoine.guerin@cyclable.com>
2025-03-12 15:33:10 +0100

    feat: add images components

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen w-screen bg-gray-50 relative">
+  <div class="h-screen w-screen bg-gray-50 relative overflow-x-hidden">
     <div
       v-if="imageFormVisible"
       class="bg-black opacity-25 absolute top-0 bottom-0 left-0 right-0 z-10"
